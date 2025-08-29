12844511e7083bfee6c2283c097d9781bf2a8f8c
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-29 10:14:51 -0400

    refactor: Toda la logica del partido y del jugador implementada en clases y con el uso de metodos

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,9 +1,49 @@
-
-
-function showScore(jugador1=0,jugador2=0){ 
-    const puntos1 = jugador1 == 1 ?  "15": jugador1 == 2 ? "30" : jugador1 == 3 ? "40" : "Love";
-    const puntos2 = jugador2 == 1 ?  "15": "Love";
-    return  `${puntos1}-${puntos2}`;
+class Jugador{
+    puntos;
+    avarage;
+    constructor(){
+        this.puntos = 0;
+        this.avarage = false;
+    }
+    anotarPunto(){
+        this.puntos ++;
+    }
+    getPuntos(){
+        return this.puntos;
+    }
+    getAvarage(){
+        return this.avarage;
+    }
+    toggleAvarage(){
+        this.avarage = !this.avarage;
+    }
 }
 
-export { showScore };
+class TenisScorer {
+    jugador1;
+    jugador2;
+    asociacionPuntos = {
+        0: "Love",
+        1: "15",
+        2: "30",
+        3: "40"
+    };
+    constructor(){
+        this.jugador1 = new Jugador();
+        this.jugador2 = new Jugador();
+    }
+    player1Scores(){
+        this.jugador1.anotarPunto();
+    }
+    player2Scores(){
+        this.jugador2.anotarPunto();
+    }
+    showScore(){ 
+        const puntos1 = this.asociacionPuntos[this.jugador1.getPuntos()];
+        const puntos2 = this.asociacionPuntos[this.jugador2.getPuntos()];
+        return  `${puntos1}-${puntos2}`;
+    }
+}
+
+
+export { TenisScorer , Jugador};
--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,16 +1,26 @@
-import { showScore } from "./tennisScorer.js";
+import { TenisScorer} from "./tennisScorer.js";
 
 describe("Tennis Scorer",()=>{
     it("El partido inicia  0-0",()=>{
-        expect(showScore()).toEqual("Love-Love");
+        let  tenisScorer = new TenisScorer();
+        expect(tenisScorer.showScore()).toEqual("Love-Love");
     });
     it("El jugador 1 anota el primer punto",()=>{
-        expect(showScore(1,0)).toEqual("15-Love");
+        let  tenisScorer = new TenisScorer();
+        tenisScorer.player1Scores();
+        expect(tenisScorer.showScore()).toEqual("15-Love");
     });
     it("El jugador 1 anota el segundo punto",()=>{
-        expect(showScore(2,0)).toEqual("30-Love");
+        let  tenisScorer = new TenisScorer();
+        tenisScorer.player1Scores();
+        tenisScorer.player1Scores();
+        expect(tenisScorer.showScore()).toEqual("30-Love");
     });
     it("El jugador 1 anota el tercer punto",()=>{
-        expect(showScore(3,0)).toEqual("40-Love");
+        let  tenisScorer = new TenisScorer();
+        tenisScorer.player1Scores();
+        tenisScorer.player1Scores();
+        tenisScorer.player1Scores();
+        expect(tenisScorer.showScore()).toEqual("40-Love");
     });
 });
