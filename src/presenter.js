12897946abe6e1509e94134e9873238256cb88df
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-29 22:29:36 -0400

    feat: Agregado vista del funcionamiento del TennisScorer

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,33 @@
-import sumar from "./sumador";
+import { TenisScorer } from "./tennisScorer.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const form = document.getElementById("tennis-form");
+const ply1 = document.getElementById("jugador1");
+const ply2 = document.getElementById("jugador2");
+const div = document.getElementById("resultado-div");
+let tenisScorer = new TenisScorer();
+let score = tenisScorer.showScore();
+ply1.addEventListener("click", () => {
+  tenisScorer.player1Scores();
+  score = tenisScorer.showScore();
+  showScore();
+});
+
+ply2.addEventListener("click", () => {
+  tenisScorer.player2Scores();
+  score = tenisScorer.showScore();
+  showScore();
+});
+
+function showScore() {
+  div.innerHTML = "<h2>" + score + "</h2>";
+}
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  showScore();
+});
+form.addEventListener("reset", (event) => {
+  tenisScorer.resetScore();
+  score = tenisScorer.showScore();
+  showScore();
 });
